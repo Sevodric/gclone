4c564be25e8034b67dbb5bad13f542975ff34101
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-04-15 23:38:29 +0200

    Refactor and comply to The Ruby Style Guide

--- rclone.rb
+++ rclone.rb
@@ -1,77 +1,91 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-# Display help if asked and exit
-if ARGV.include?("--help") || ARGV.length < 1
-  puts "Usage: rclone GITHUB-ID/REPOSITORY...       Use the given GitHub ID"
-  puts "       rclone REPOSITORY...                 Use the default GitHub ID"
-  puts "       rclone GITHUB-URL...                 Use the full GitHub URL"
-  puts "       rclone --set-default GITHUB-ID       Set the default GitHub ID"
-  puts "       rclone --help                        Display this help"
-  puts "\nQuickly clone one or more repositories from GitHub"
+# Displays help if asked and exit
+if ARGV.include?('--help') || ARGV.empty?
+  puts 'Usage: rclone GITHUB-ID/REPOSITORY...       Use the given GitHub ID'
+  puts '       rclone REPOSITORY...                 Use the default GitHub ID'
+  puts '       rclone GITHUB-URL...                 Use the full GitHub URL'
+  puts '       rclone --set-default GITHUB-ID       Set the default GitHub ID'
+  puts '       rclone --help                        Display this help'
+  puts 'Quickly clone one or more repositories from GitHub'
   exit
 end
 
-GIT_ERROR_MSG = "[ERROR] 'git' nor found.\nConsider checking for correct " \
-  "installation of the 'git' package. Refer to your distribution's package " \
-  "manager for further installation details."
+GIT_ERROR_MSG = '[ERROR] `git` nor found. Consider checking for correct ' \
+  'installation of the `git` package. Refer to your distribution\'s package ' \
+  'manager for further installation details.'
 
 INTERRUPT_MSG = "\n[WARNING] Cloning interrupted."
 
 # The configuration file uses the standard $HOME/.config/ directory and contains
 # nothing more than the default GitHub ID
-CONFIG_DIR = File.join(Dir.home, ".config", "rclone")
-CONFIG_PATH = File.join(CONFIG_DIR, "config")
+CONFIG_DIR = File.join(Dir.home, '.config', 'rclone')
+CONFIG_PATH = File.join(CONFIG_DIR, 'config')
 
-# Ensure the "~/.config/rclone/" directory does exist
-Dir.mkdir(CONFIG_DIR) if !Dir.exist?(CONFIG_DIR)
+# Ensures the '~/.config/rclone/' directory does exist
+Dir.mkdir(CONFIG_DIR) unless Dir.exist?(CONFIG_DIR)
 
-# Main loop
-ARGV.each_with_index do |arg, index|
+# Sets the default GitHub ID.
+def update_default_id(id)
+  File.open(CONFIG_PATH, 'w') { |f| f.write(id) }
+  puts "Default GitHub ID set to: #{id}"
+  exit
+end
 
-  # Set default GitHub ID if asked and exit
-  if arg == "--set-default"
-    File.open(CONFIG_PATH, "w") { |f| f.write(ARGV[index + 1]) }
-    puts "Default GitHub ID set to: '#{ARGV[index + 1]}'"
-    exit
+# Clones from a full repo link.
+def clone_from_full_link(link)
+  puts "[INFO] Cloning from #{link}"
+  begin
+    puts GIT_ERROR_MSG if system("git clone #{link}").nil?
+  rescue Interrupt
+    puts INTERRUPT_MSG
   end
+end
 
-  # Clone the repository from the current argument
-  if /https:\/\/github\.com\/[[:ascii:]]+\/[[:ascii:]]+\.git/.match?(arg)
-    puts "cloning from #{arg}"
-    begin
-      if system("git clone #{arg}") == nil
-        puts GIT_ERROR_MSG
-      end
-    rescue Interrupt
-      puts INTERRUPT_MSG
+# Clones from an argument of the form id/repo.
+def clone_from_couple(arg)
+  puts "[INFO] Cloning from '#{arg}'"
+  begin
+    puts GIT_ERROR_MSG if system("git clone https://github.com/#{arg}").nil?
+  rescue Interrupt
+    puts INTERRUPT_MSG
+  end
+end
+
+# Clones from the default GitHub ID.
+def clone_from_default(repo)
+  github_id = retrieve_default_id
+  puts "[INFO] Cloning from '#{github_id}/#{repo}' (default GitHub ID)"
+  begin
+    if system("git clone https://github.com/#{github_id}/#{repo}").nil?
+      puts GIT_ERROR_MSG
     end
-  elsif /\A[[:alnum:]]+\/[[:alnum:]]+/.match?(arg)
-    puts "Cloning from '#{arg}'"
-    begin
-      if system("git clone https://github.com/#{arg}") == nil
-        puts GIT_ERROR_MSG
-      end
-    rescue Interrupt
-      puts INTERRUPT_MSG
-    end
-  else  
-    # If the default GitHub ID doesn't exist yet, ask for it
-    if !File.exist?(CONFIG_PATH)
-      puts "No default ID found, please enter a default GitHub ID..."
-      github_id = STDIN.gets.chomp
-      File.open(CONFIG_PATH, "w") { |f| f.write(github_id) }
-      puts "Default GitHub ID set to: '#{github_id}'"
-    else
-      # Get the default GitHub ID from the config file
-      github_id = File.read(CONFIG_PATH)
-    end
-    puts "Cloning from '#{github_id}/#{arg}'"
-    begin
-      if system("git clone https://github.com/#{github_id}/#{arg}") == nil
-        puts GIT_ERROR_MSG
-      end
-    rescue Interrupt
-      puts INTERRUPT_MSG
-    end
+  rescue Interrupt
+    puts INTERRUPT_MSG
+  end
+end
+
+# Returns the default GitHub ID.
+def retrieve_default_id
+  if File.exist?(CONFIG_PATH)
+    github_id = File.read(CONFIG_PATH)
+  else
+    puts 'No default ID found, please enter a default GitHub ID...'
+    github_id = STDIN.gets.chomp
+    File.open(CONFIG_PATH, 'w') { |f| f.write(github_id) }
+    puts "Default GitHub ID set to: '#{github_id}'"
+  end
+end
+
+ARGV.each_with_index do |arg, index|
+  if arg == '--set-default'
+    update_default_id(ARGV[index + 1])
+  elsif %r{https://github\.com/[[:ascii:]]+/[[:ascii:]]+\.git}.match?(arg)
+    clone_from_full_link(arg)
+  elsif %r{\A[[:alnum:]]+/[[:alnum:]]+}.match?(arg)
+    clone_from_couple(arg)
+  else
+    clone_from_default(arg)
   end
 end
