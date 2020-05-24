01b481a9831b129f7d8076e363d3302a456a9878
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-05-24 17:41:55 +0200

    Change install.sh to explicitly require sudo

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
-cp gclone.rb gclone
-chmod +x gclone
-sudo mv gclone /usr/bin/gclone
+if (($EUID != 0))
+    then echo "[ERROR] Requires root privileges."
+else
+    cp gclone.rb gclone
+    chmod +x gclone
+    mv gclone /usr/bin/gclone
+    echo "gclone has successfully been installed."
+fi
